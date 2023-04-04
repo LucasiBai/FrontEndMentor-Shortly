f5b647dcdf06c451cbdd0cc7f4bad66c3a1e2115
LucasiBai <lucasibaigorria@mi.unc.edu.ar>
2023-04-04 19:38:50 -0300

    agregado de elementos en home page + agregado de estilos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationModule } from './modules/navigation/navigation.module';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ButtonsModule } from './modules/buttons/buttons.module';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, NavigationModule],
+  declarations: [AppComponent, HomePageComponent],
+  imports: [BrowserModule, AppRoutingModule, NavigationModule, ButtonsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
