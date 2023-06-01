a9732d391e755d671ecc4bf0227b4c4427a32ad0
LucasiBai <lucasibaigorria@mi.unc.edu.ar>
2023-06-01 20:06:41 -0300

    terminando estilos responsive en botones

--- src/app/modules/buttons/components/text-button/text-button.component.ts
+++ src/app/modules/buttons/components/text-button/text-button.component.ts
@@ -1,14 +1,30 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-text-button',
   templateUrl: './text-button.component.html',
   styleUrls: ['./text-button.component.scss'],
 })
-export class TextButtonComponent implements OnInit {
+export class TextButtonComponent implements OnInit, AfterViewInit {
   @Input() type!: string;
+  @Input() fontSize!: number;
+
+  @ViewChild('button') button!: ElementRef;
 
   ngOnInit(): void {
     !this.type && (this.type = 'fat');
+  }
+
+  ngAfterViewInit(): void {
+    if (this.fontSize !== undefined) {
+      this.button.nativeElement.style.fontSize = `${this.fontSize}px`;
+    }
   }
 }
